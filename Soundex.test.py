a4d33cdeca2e0e7a8434bb9faeae7cc44541a7d0
bijayv <nrbijay52@gmail.com>
2024-07-05 14:54:09 +0530

    Update Soundex.test.py

--- Soundex.test.py
+++ Soundex.test.py
@@ -6,6 +6,12 @@ class TestSoundex(unittest.TestCase):
     def test_empty_string(self):
         self.assertEqual(generate_soundex(""), "")
         self.assertEqual(generate_soundex("A"), "A000")
+        self.assertEqual(generate_soundex("Robert"), "R163")
+        self.assertEqual(generate_soundex("Rupert"), "R163")
+        self.assertEqual(generate_soundex("Rubin"), "R150")
+        self.assertEqual(generate_soundex("Ashcraft"), "A261")
+        self.assertEqual(generate_soundex("Ashcroft"), "A261")
+        self.assertEqual(generate_soundex("Pfister"), "P236")
         
     def test_single_character(self):
         self.assertEqual(generate_soundex("A"), "A000")
